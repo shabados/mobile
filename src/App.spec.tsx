b28bbe17fb458b2aec6382143d9b265e2231f6a3
Harjot Singh <harjot@harkul.com>
2020-04-12 11:07:39 +0100

    test: move tests to be inline with components

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import App from './App'
+
+it( 'renders correctly', () => {
+  renderer.create( <App /> )
+} )
