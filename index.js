f212c41d0934408a9a24f2b86028c7af6ac463f8
Harjot Singh <harjot@harkul.com>
2020-04-12 13:57:55 +0100

    feat: enable native screens for navigation

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { AppRegistry } from 'react-native'
 import 'react-native-gesture-handler'
+import { enableScreens } from 'react-native-screens'
 
 import App from './src/App'
 import { name as appName } from './app.json'
+
+enableScreens()
 
 AppRegistry.registerComponent( appName, () => App )
