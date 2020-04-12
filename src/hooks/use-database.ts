a715dd15575a4c3001fe3c0b7046b018883a88d4
Harjot Singh <harjot@harkul.com>
2020-04-12 13:07:57 +0100

    refactor: switch hooks to use arrow-functions

--- src/hooks/use-database.ts
+++ src/hooks/use-database.ts
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
+
 import { DatabaseContext } from '../App'
 
-export default function useDatabase() {
-  return useContext( DatabaseContext )
-}
+export default () => useContext( DatabaseContext )
