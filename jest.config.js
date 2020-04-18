529bb217ddccd108060e779765b46a8563290c0d
Harjot Singh <harjot@harkul.com>
2020-04-18 19:36:50 +0100

    build: add typescript preprocessing to jest test

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,30 @@
+const { defaults: tsjPreset } = require( 'ts-jest/presets' )
+
+// List of modules that do not transpile their code
+const whitelistedModules = [
+  'react-native',
+  'native-base',
+]
+
 module.exports = {
+  ...tsjPreset,
   preset: 'react-native',
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
+  collectCoverage: true,
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  transformIgnorePatterns: [
+    `node_modules/(?!${whitelistedModules.join( '|' )})`,
   ],
+  setupFiles: [
+    'react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/test/setupJest.js',
+  ],
+  moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
 }
