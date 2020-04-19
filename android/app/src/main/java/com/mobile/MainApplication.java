1a60543acdcb9b9b64f619a74cdab6f26fd2027e
Harjot Singh <harjot@harkul.com>
2020-04-19 15:50:34 +0100

    build: link android native dependencies
    Includes fonts, watermelonDB

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.nozbe.watermelondb.WatermelonDBPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -23,8 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+
+          // Packages that need to be manually linked
+          packages.add(new WatermelonDBPackage());
+
           return packages;
         }
 
