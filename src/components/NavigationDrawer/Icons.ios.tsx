3437fd03d6f168debbf0ba45a47a06de4d7417c7
saihaj <44710980+saihaj@users.noreply.github.com>
2020-08-20 12:53:42 -0500

    feat: setup platform specifc icons for nav drawer

---
+++ src/components/NavigationDrawer/Icons.ios.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createIconSetFromFontello } from 'react-native-vector-icons'
+import fontelloConfig from '../../../assets/fonts/fontello-icons.json'
+
+const Icon = createIconSetFromFontello( fontelloConfig )
+
+const ICON_SIZE = 20
+const ICON_COLOR = 'rgb(47,124,246)'
+const navigationIconSpacing = { paddingRight: '8%', paddingLeft: '8%' }
+
+export default {
+  SearchIcon: ( props: any ) => <Icon name="search" size={ICON_SIZE} color={ICON_COLOR} style={navigationIconSpacing} {...props} />,
+  HistoryIcon: ( props: any ) => <Icon name="history" size={ICON_SIZE} color={ICON_COLOR} style={navigationIconSpacing} {...props} />,
+  BookmarkIcon: ( props: any ) => <Icon name="bookmark" size={ICON_SIZE} color={ICON_COLOR} style={navigationIconSpacing} {...props} />,
+  TabsIcon: ( props: any ) => <Icon name="tabs" size={ICON_SIZE} color={ICON_COLOR} style={navigationIconSpacing} {...props} />,
+  DotsIcon: ( props: any ) => <Icon name="ellipsis" size={ICON_SIZE} color={ICON_COLOR} style={navigationIconSpacing} {...props} />,
+}
