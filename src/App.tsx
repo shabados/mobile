a40ecd47fd2f42cf5ff4609bb15bd33da3ff2491
Harjot Singh <harjot@harkul.com>
2020-09-10 16:45:37 +0100

    feat: remove Data Manager

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import HomeScreen from './screens/HomeScreen'
 import SearchScreen from './screens/SearchScreen'
-import DataManager from './screens/DataManager'
 
 import { SEARCH_SCREEN, HOME_SCREEN } from './lib/screens'
 
@@ -20,7 +19,6 @@ const RootNavigator = (): JSX.Element => (
   <Drawer.Navigator>
     <Drawer.Screen name={HOME_SCREEN} component={HomeScreen} />
     <Drawer.Screen name={SEARCH_SCREEN} component={SearchScreen} />
-    <Drawer.Screen name="DataManager" component={DataManager} />
   </Drawer.Navigator>
 )
 
