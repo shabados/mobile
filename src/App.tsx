ecc2b774c2b78092a99cf45fd564d38cab3be17a
saihaj <saihajpreet.singh@gmail.com>
2020-09-09 21:18:02 -0500

    refactor: remove native base

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { NavigationContainer, DrawerActions } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-import { Container, Header, Content, List, ListItem, Text } from 'native-base'
 
 import HomeScreen from './screens/HomeScreen'
 import SearchScreen from './screens/SearchScreen'
+import DataManager from './screens/DataManager'
 
 import { SEARCH_SCREEN, HOME_SCREEN } from './lib/screens'
-import DataManager from './screens/DataManager'
 
 const Drawer = createDrawerNavigator()
 
@@ -17,29 +16,8 @@ const App = () => (
   </NavigationContainer>
 )
 
-const DrawerContent = ( props: any ) => {
-  const goTo = ( destination: string ) => {
-    props.navigation.dispatch( DrawerActions.jumpTo( destination ) )
-  }
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <List>
-          <ListItem>
-            <Text onPress={() => goTo( 'Home' )}>Home</Text>
-          </ListItem>
-          <ListItem>
-            <Text onPress={() => goTo( 'DataManager' )}>DataManager</Text>
-          </ListItem>
-        </List>
-      </Content>
-    </Container>
-  )
-}
-
-const RootNavigator = () => (
-  <Drawer.Navigator drawerContent={( props ) => DrawerContent( props )}>
+const RootNavigator = (): JSX.Element => (
+  <Drawer.Navigator>
     <Drawer.Screen name={HOME_SCREEN} component={HomeScreen} />
     <Drawer.Screen name={SEARCH_SCREEN} component={SearchScreen} />
     <Drawer.Screen name="DataManager" component={DataManager} />
