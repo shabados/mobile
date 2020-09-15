5b3877077a3f204cb5fdc66583c5166372da0bd6
Harjot Singh <harjot@harkul.com>
2020-09-15 19:11:11 +0100

    test: use fake timers on all jest tests by default

--- test/setupJest.js
+++ test/setupJest.js
@@ -1,5 +1,17 @@
 // Mock out native animations
-jest.mock(
-  'react-native-reanimated',
-  () => jest.requireActual( 'react-native-reanimated/mock' ),
-)
+import 'react-native-gesture-handler/jestSetup'
+
+jest.mock( 'react-native-reanimated', () => {
+  const Reanimated = require( 'react-native-reanimated/mock' )
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {}
+  return Reanimated
+} )
+
+// Silence the warning:
+// Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock( 'react-native/Libraries/Animated/src/NativeAnimatedHelper' )
+
+jest.useFakeTimers()
