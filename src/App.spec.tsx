26189ad3752aabd822b855f5e5a04676120d3bd7
Harjot Singh <harjot@harkul.com>
2020-09-15 19:10:56 +0100

    test: restructure App smoke test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -5,7 +5,9 @@ import renderer from 'react-test-renderer'
 import App from './App'
 
 describe( 'App', () => {
-  it( 'renders correctly', () => {
-    renderer.create( <App /> )
+  describe( 'Initial load', () => {
+    it( 'Renders without crashing', async () => {
+      renderer.create( <App /> )
+    } )
   } )
 } )
