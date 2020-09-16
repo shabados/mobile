d36acc4fa88218151d4a3b27c9564d30ca861395
saihaj <saihajpreet.singh@gmail.com>
2020-09-16 16:46:36 -0500

    feat: setup stack navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import HomeScreen from './screens/HomeScreen'
 import SearchScreen from './screens/SearchScreen'
 
 import Screens from './lib/screens'
 
-const Drawer = createDrawerNavigator()
+const Stack = createStackNavigator()
 
 const App = () => (
   <NavigationContainer>
-    <Drawer.Navigator>
-      <Drawer.Screen name={Screens.Home} component={HomeScreen} />
-      <Drawer.Screen name={Screens.Search} component={SearchScreen} />
-    </Drawer.Navigator>
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name={Screens.Home} component={HomeScreen} />
+      <Stack.Screen name={Screens.Search} component={SearchScreen} />
+    </Stack.Navigator>
   </NavigationContainer>
 )
 
