40aa82272312ff4d3596453d895599c548892774
saihaj <saihajpreet.singh@gmail.com>
2020-09-17 14:03:44 -0500

    feat: use safe area context in `Container`
    related #112

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import HomeScreen from './screens/HomeScreen'
 import SearchScreen from './screens/SearchScreen'
@@ -10,12 +11,14 @@ import Screens from './lib/screens'
 const Stack = createStackNavigator()
 
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen name={Screens.Home} component={HomeScreen} />
-      <Stack.Screen name={Screens.Search} component={SearchScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name={Screens.Home} component={HomeScreen} />
+        <Stack.Screen name={Screens.Search} component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </SafeAreaProvider>
 )
 
 export default App
