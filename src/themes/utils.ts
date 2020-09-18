a790fe920b7e979f3751fb5d9af95b62b3e7d9cd
saihaj <saihajpreet.singh@gmail.com>
2020-09-18 15:49:47 -0500

    refactor: move centering logic to a shared util

---
+++ src/themes/utils.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const StyleUtils = StyleSheet.create( {
+  /**
+   * Set Top and Bottom margin to auto
+   */
+  mx: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+} )
+
+export default StyleUtils
