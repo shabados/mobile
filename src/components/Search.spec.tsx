f1f9774a1e41a60584e61f9cd82e1a6f70871069
Harjot Singh <harjot@harkul.com>
2020-11-25 17:18:35 +0000

    refactor: rename Search props

--- src/components/Search.spec.tsx
+++ src/components/Search.spec.tsx
@@ -1,6 +1,6 @@
 import 'react-native'
 import React from 'react'
-import { render, fireEvent } from 'react-native-testing-library'
+import { render, fireEvent } from '@testing-library/react-native'
 
 import SearchBar from './Search'
 
@@ -8,11 +8,12 @@ describe( 'Search', () => {
   it( 'Handle user text input', () => {
     const onEventMock = jest.fn()
 
-    const { getByPlaceholder } = render(
-      <SearchBar handleTextChanges={onEventMock} />,
+    const { getByPlaceholderText } = render(
+      <SearchBar onChangeText={onEventMock} />,
     )
 
-    fireEvent( getByPlaceholder( 'Koj' ), 'onChangeText', 'Test Input' )
+    fireEvent.changeText( getByPlaceholderText( 'Koj' ), 'Test Input' )
+
     expect( onEventMock ).toHaveBeenCalledWith( 'Test Input' )
   } )
 } )
