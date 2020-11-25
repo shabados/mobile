4f327c60d92ddb6e916ba837ee3364ea1812a3d0
Harjot Singh <harjot@harkul.com>
2020-11-25 17:34:04 +0000

    style: remove ununsed act imports from tests

--- src/components/BackButton.spec.tsx
+++ src/components/BackButton.spec.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack'
-import { act } from 'react-test-renderer'
 
 import BackButton, { BackButtonProps } from './BackButton'
 
