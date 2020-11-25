d56eb1595ae1ee54c2f08b98e4c05de06376b425
Harjot Singh <harjot@harkul.com>
2020-11-25 18:18:11 +0000

    test: add change language button test

--- src/components/Search.spec.tsx
+++ src/components/Search.spec.tsx
@@ -4,16 +4,29 @@ import { render, fireEvent } from '@testing-library/react-native'
 
 import SearchBar from './Search'
 
-describe( 'Search', () => {
-  it( 'Handle user text input', () => {
+describe( '<Search />', () => {
+  it( 'should handle user text input', () => {
     const onEventMock = jest.fn()
 
     const { getByPlaceholderText } = render(
       <SearchBar onChangeText={onEventMock} />,
     )
 
-    fireEvent.changeText( getByPlaceholderText( 'Koj' ), 'Test Input' )
+    fireEvent.changeText( getByPlaceholderText( 'Search' ), 'Test Input' )
 
     expect( onEventMock ).toHaveBeenCalledWith( 'Test Input' )
+  } )
+
+  it( 'should change language, when the language button is pressed', async () => {
+    const {
+      getByText,
+      getByPlaceholderText,
+      findByPlaceholderText,
+    } = render( <SearchBar onChangeText={jest.fn()} /> )
+
+    expect( getByPlaceholderText( 'Search' ) ).toBeTruthy()
+    fireEvent.press( getByText( 'PA' ) )
+
+    expect( await findByPlaceholderText( 'ਖੌਜ' ) ).toBeTruthy()
   } )
 } )
