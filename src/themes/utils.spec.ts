afbbf5c9ee5ddb8cf77a4f1e6e0435b3fb991bd2
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-11-30 16:44:45 -0600

    feat: utilities for padding

---
+++ src/themes/utils.spec.ts
@@ -0,0 +1,19 @@
+import { px, py } from './utils'
+
+describe( 'Styling Utilities', () => {
+  it( 'should have paddingBottom and paddingTop set to auto', () => {
+    expect( px() ).toStrictEqual( { paddingTop: 'auto', paddingBottom: 'auto' } )
+  } )
+
+  it( 'should have paddingBottom and paddingTop set to 10', () => {
+    expect( px( 10 ) ).toStrictEqual( { paddingTop: 10, paddingBottom: 10 } )
+  } )
+
+  it( 'should have paddingLeft and paddingRight set to auto', () => {
+    expect( py() ).toStrictEqual( { paddingLeft: 'auto', paddingRight: 'auto' } )
+  } )
+
+  it( 'should have paddingLeft and paddingRight set to 10', () => {
+    expect( py( 10 ) ).toStrictEqual( { paddingLeft: 10, paddingRight: 10 } )
+  } )
+} )
--- src/themes/utils.ts
+++ src/themes/utils.ts
@@ -5,3 +5,22 @@ export const mx = {
   marginTop: 'auto',
   marginBottom: 'auto',
 }
+
+/**
+ * Set Top and Bottom padding to given value.
+ * Default: 'auto'
+ */
+export const px = ( value: number|string = 'auto' ) => ( {
+  paddingTop: value,
+  paddingBottom: value,
+} )
+
+/**
+ * Set Left and Right padding to given value.
+ * Default: 'auto'
+ */
+export const py = ( value: number|string = 'auto' ) => ( {
+  paddingLeft: value,
+  paddingRight: value,
+} )
+
