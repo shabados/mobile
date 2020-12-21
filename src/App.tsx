916061b23d65bde5265a5fa30d5de62a33e2dc8e
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-20 22:51:41 -0600

    refactor: use bookmark enum

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     >
       <Stack.Screen name={Screens.Home} component={HomeScreen} />
       <Stack.Screen name={Screens.Search} component={SearchScreen} />
-      <Stack.Screen name="Bookmarks" component={Bookmarks} options={{ stackPresentation: 'modal' }} />
+      <Stack.Screen name={Screens.Bookmarks} component={Bookmarks} options={{ stackPresentation: 'modal' }} />
     </Stack.Navigator>
   </NavigationContainer>
 )
