d0b908b43b738eb69da60d852cd27e41ea2e3f05
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-30 07:11:58 -0600

    test: use toEqual instead of strict equal

--- src/themes/utils.spec.ts
+++ src/themes/utils.spec.ts
@@ -2,18 +2,18 @@ import { px, py } from './utils'
 
 describe( 'Styling Utilities', () => {
   it( 'should have paddingBottom and paddingTop set to auto', () => {
-    expect( py() ).toStrictEqual( { paddingTop: 'auto', paddingBottom: 'auto' } )
+    expect( py() ).toEqual( { paddingTop: 'auto', paddingBottom: 'auto' } )
   } )
 
   it( 'should have paddingBottom and paddingTop set to 10', () => {
-    expect( py( 10 ) ).toStrictEqual( { paddingTop: 10, paddingBottom: 10 } )
+    expect( py( 10 ) ).toEqual( { paddingTop: 10, paddingBottom: 10 } )
   } )
 
   it( 'should have paddingLeft and paddingRight set to auto', () => {
-    expect( px() ).toStrictEqual( { paddingLeft: 'auto', paddingRight: 'auto' } )
+    expect( px() ).toEqual( { paddingLeft: 'auto', paddingRight: 'auto' } )
   } )
 
   it( 'should have paddingLeft and paddingRight set to 10', () => {
-    expect( px( 10 ) ).toStrictEqual( { paddingLeft: 10, paddingRight: 10 } )
+    expect( px( 10 ) ).toEqual( { paddingLeft: 10, paddingRight: 10 } )
   } )
 } )
