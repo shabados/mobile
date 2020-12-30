6e0d990e4763da6bffd15ed7cfb6801d677b1ff2
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-29 20:04:33 -0600

    feat: search result component

---
+++ src/components/Search/Result.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Text, StyleSheet, View, Pressable, PressableProps, ViewStyle } from 'react-native'
+
+import Colours from '../../themes/colours'
+
+const styles = StyleSheet.create( {
+  container: {
+    padding: 5,
+    backgroundColor: Colours.MediumGray,
+    borderBottomColor: Colours.LightGray,
+    borderBottomWidth: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 15,
+    fontWeight: '400',
+  },
+  line: {
+    marginBottom: 5,
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  translation: {
+    fontSize: 15,
+  },
+} )
+
+export type SearchResultDataProps = {
+  /**
+   * Name of gurbani source
+   */
+  source: string,
+  /**
+   * Page number in source
+   */
+  page: string,
+  /**
+   * Last accessed date
+   */
+  date?: string,
+  /**
+   * Gurbani Line
+   */
+  line: string,
+  /**
+   * Translation for gurbani line
+   */
+  translation: string,
+}
+
+export type SearchResultProps = {
+  /**
+   * Style for `View` container
+   */
+  customStyle?: ViewStyle,
+  onPress: NonNullable<PressableProps['onPress']>,
+} & Omit<PressableProps, 'style'> & SearchResultDataProps
+
+const SearchResult = ( {
+  source,
+  page,
+  date,
+  line,
+  translation,
+  onPress,
+  customStyle,
+  ...props
+}:SearchResultProps ) => (
+  <Pressable onPress={onPress} {...props}>
+    <View style={[ styles.container, customStyle ]}>
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{source}</Text>
+        <Text>{page}</Text>
+        { date && <Text>{date}</Text>}
+      </View>
+
+      <Text style={styles.line}>{line}</Text>
+
+      <Text style={styles.translation}>{translation}</Text>
+    </View>
+  </Pressable>
+)
+
+export default SearchResult
--- src/components/Search/index.ts
+++ src/components/Search/index.ts
@@ -1,1 +1,2 @@
 export { default as SearchBar } from './Bar'
+export { default as SearchResult } from './Result'
