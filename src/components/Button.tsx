b4d3a9428ec7d04417eff711653b86e17db17d86
Harjot Singh <harjot@harkul.com>
2021-01-02 04:19:34 +0000

    feat: add themed button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react'
+import { Pressable, PressableProps, StyleSheet, ViewProps } from 'react-native'
+
+import Colours from '../themes/colours'
+import { mx } from '../themes/utils'
+
+import Typograpy from './Typography'
+
+const styles = StyleSheet.create( {
+  pressed: {
+    opacity: 0.6,
+  },
+  root: {
+    borderRadius: 10,
+    backgroundColor: Colours.LightGray,
+    width: 75,
+    ...mx( 10 ),
+    height: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+} )
+
+export type ButtonProps = PressableProps & { style?: ViewProps }
+
+/**
+ * Themed button component, pressable.
+ */
+const Button = ( { style, children, ...props }: ButtonProps ) => (
+  <Pressable
+    style={( { pressed } ) => [ styles.root, pressed && styles.pressed, style ]}
+    {...props}
+  >
+    <Typograpy variant="button">
+      {children}
+    </Typograpy>
+  </Pressable>
+)
+
+export default Button
