40a34859cb3c6071acb05cb790e692f17f9b0645
Harjot Singh <harjot@harkul.com>
2021-01-02 04:15:44 +0000

    feat: add mx and my theme utilities

--- src/themes/utils.spec.ts
+++ src/themes/utils.spec.ts
@@ -1,19 +1,43 @@
-import { px, py } from './utils'
+import { mx, my, px, py } from './utils'
 
 describe( 'Styling Utilities', () => {
-  it( 'should have paddingBottom and paddingTop set to auto', () => {
-    expect( py() ).toEqual( { paddingTop: 'auto', paddingBottom: 'auto' } )
+  describe( 'py()', () => {
+    it( 'given no value, should have paddingBottom and paddingTop set to auto', () => {
+      expect( py() ).toEqual( { paddingTop: 'auto', paddingBottom: 'auto' } )
+    } )
+
+    it( 'given a value of 10, should have paddingBottom and paddingTop set to 10', () => {
+      expect( py( 10 ) ).toEqual( { paddingTop: 10, paddingBottom: 10 } )
+    } )
   } )
 
-  it( 'should have paddingBottom and paddingTop set to 10', () => {
-    expect( py( 10 ) ).toEqual( { paddingTop: 10, paddingBottom: 10 } )
+  describe( 'px()', () => {
+    it( 'given no value, should have paddingLeft and paddingRight set to auto', () => {
+      expect( px() ).toEqual( { paddingLeft: 'auto', paddingRight: 'auto' } )
+    } )
+
+    it( 'given a value of 10, should have paddingLeft and paddingRight set to 10', () => {
+      expect( px( 10 ) ).toEqual( { paddingLeft: 10, paddingRight: 10 } )
+    } )
   } )
 
-  it( 'should have paddingLeft and paddingRight set to auto', () => {
-    expect( px() ).toEqual( { paddingLeft: 'auto', paddingRight: 'auto' } )
+  describe( 'mx()', () => {
+    it( 'given no value, should have marginLeft and marginLeft set to auto', () => {
+      expect( mx() ).toEqual( { marginLeft: 'auto', marginRight: 'auto' } )
+    } )
+
+    it( 'given a value of 10, should have paddingLeft and paddingRight set to 10', () => {
+      expect( mx( 10 ) ).toEqual( { marginLeft: 10, marginRight: 10 } )
+    } )
   } )
 
-  it( 'should have paddingLeft and paddingRight set to 10', () => {
-    expect( px( 10 ) ).toEqual( { paddingLeft: 10, paddingRight: 10 } )
+  describe( 'my()', () => {
+    it( 'given no value, should have marginTop and marginBottom set to auto', () => {
+      expect( my() ).toEqual( { marginTop: 'auto', marginBottom: 'auto' } )
+    } )
+
+    it( 'given a value of 10, should have marginTop and marginBottom set to 10', () => {
+      expect( my( 10 ) ).toEqual( { marginTop: 10, marginBottom: 10 } )
+    } )
   } )
 } )
--- src/themes/utils.ts
+++ src/themes/utils.ts
@@ -1,14 +1,21 @@
 /**
- * Set Top and Bottom margin to auto
+ * Set Top and Bottom margin to given value.
  */
-export const my = {
-  marginTop: 'auto',
-  marginBottom: 'auto',
-}
+export const my = ( value: number|string = 'auto' ) => ( {
+  marginTop: value,
+  marginBottom: value,
+} )
+
+/**
+ * Set Left and Right margin to given value.
+ */
+export const mx = ( value: number|string = 'auto' ) => ( {
+  marginLeft: value,
+  marginRight: value,
+} )
 
 /**
  * Set Left and Right padding to given value.
- * Default: 'auto'
  */
 export const px = ( value: number|string = 'auto' ) => ( {
   paddingLeft: value,
@@ -17,7 +24,6 @@ export const px = ( value: number|string = 'auto' ) => ( {
 
 /**
  * Set Top and Bottom padding to given value.
- * Default: 'auto'
  */
 export const py = ( value: number|string = 'auto' ) => ( {
   paddingTop: value,
