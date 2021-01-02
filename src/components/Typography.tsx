445916a1d2b52bd47282e0acc1e70698f7b0e530
Harjot Singh <harjot@harkul.com>
2021-01-02 04:14:32 +0000

    refactor: add generic Typography component

---
+++ src/components/Typography.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react'
+import { StyleSheet, Text, TextProps } from 'react-native'
+
+import Colours from '../themes/colours'
+import Fonts from '../themes/fonts'
+
+type Variants = 'body' | 'header' | 'button'
+
+export type TypographyProps = TextProps & {
+  children: ReactNode,
+  variant?: Variants,
+}
+
+const styles = StyleSheet.create( {
+  base: {
+    color: Colours.White,
+    fontFamily: Fonts.NotoSansRegular,
+  },
+  body: {},
+  button: {
+    color: Colours.TintedWhite,
+  },
+  header: {
+    fontSize: 18,
+    fontFamily: Fonts.NotoSansLight,
+  },
+} )
+
+/**
+ * Typography component with different variants.
+ */
+const Typograpy = ( {
+  style,
+  variant = 'body',
+  ...props
+}: TypographyProps ) => <Text style={[ styles.base, styles[ variant ], style ]} {...props} />
+
+export default Typograpy
