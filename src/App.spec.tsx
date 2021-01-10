e2e4a6bd28431dd220117f3a4cff9643168e7ce1
Harjot Singh <harjot@harkul.com>
2021-01-10 20:05:26 +0000

    test: remove unnecessary import 'react-native'

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,3 @@
-import 'react-native'
 import React from 'react'
 import { render } from '@testing-library/react-native'
 
--- src/components/Search/Bar.spec.tsx
+++ src/components/Search/Bar.spec.tsx
@@ -1,4 +1,3 @@
-import 'react-native'
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react-native'
 
