4be2618d0c4ae2a3ea8d6b785fdb397909af339e
Harjot Singh <harjot@harkul.com>
2021-01-10 18:50:27 +0000

    test: add integration tests for Gurbani screen

---
+++ src/screens/Gurbani/index.int.spec.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { render } from '@testing-library/react-native'
+import { when } from 'jest-when'
+
+import withContexts from '../../components/with-contexts'
+import * as shabads from '../../data/shabads'
+
+import GurbaniScreen from './index'
+
+const Stack = createStackNavigator()
+
+type WrapperProps = { children: ReactNode }
+const wrapper = ( { children }: WrapperProps ) => withContexts(
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="default">{() => children}</Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>,
+)
+
+describe( '<GurbaniScreen />', () => {
+  describe( 'on mount', () => {
+    const getShabadMock = jest.spyOn( shabads, 'getShabad' )
+    when( getShabadMock )
+      .calledWith( 'DMP' )
+      .mockResolvedValue( [
+        { id: '123', gurmukhi: 'test-gurmukhi', translations: [] },
+      ] )
+
+    it( 'should render a bottom bar', () => {
+      const { unmount, getByPlaceholderText } = render( <GurbaniScreen />, { wrapper } )
+
+      expect( getByPlaceholderText( 'Search' ) ).toBeTruthy()
+
+      unmount()
+    } )
+
+    it( 'should load and render a target shabad', async () => {
+      const { unmount, findByText } = render( <GurbaniScreen />, { wrapper } )
+
+      expect( await findByText( 'test-gurmukhi' ) ).toBeTruthy()
+
+      unmount()
+    } )
+  } )
+} )
