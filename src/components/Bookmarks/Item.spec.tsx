f8fb8719d6eace87fd9bfde2db742da23b4bde2b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-01-13 21:08:33 -0600

    test: bookmark item

---
+++ src/components/Bookmarks/Item.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react-native'
+
+import BookmarkItem from './Item'
+
+describe( '<BookmarkItem />', () => {
+  it( 'given a press, should fire onPress with details of the bookmark item', () => {
+    const onPress = jest.fn()
+
+    const { getByText } = render(
+      <BookmarkItem title="Test Me" isFolder={false} onPress={onPress} />,
+    )
+
+    fireEvent.press( getByText( 'Test Me' ) )
+
+    expect( onPress ).toHaveBeenCalled()
+  } )
+} )
