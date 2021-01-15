98eb389509ea4885f03376f0e39bb71678349eaf
Harjot Singh <harjot@harkul.com>
2021-01-15 01:22:34 +0000

    feat: add Search screen Navbar

---
+++ src/screens/Search/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+import Colours from '../../themes/colours'
+import { px } from '../../themes/utils'
+import Navbar from '../../components/Navbar'
+import SearchBar, { SearchBarProps } from '../../components/SearchBar'
+import BackButton from '../../components/BackButton'
+
+const styles = StyleSheet.create( {
+  back: {
+    width: 30,
+  },
+  backIcon: {
+    fontSize: 20,
+  },
+  root: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  searchBar: {
+    flex: 1,
+    marginRight: 20,
+  },
+} )
+
+export type SearchNavbarProps = {
+  /**
+   * Fired with the text value when the search changes.
+   */
+  onSearchChange?: SearchBarProps['onChangeText'],
+}
+
+/**
+ * Navbar component for main header.
+ */
+const SearchNavbar = ( {
+  onSearchChange = () => {},
+}: SearchNavbarProps ) => (
+  <Navbar
+    backgroundColor="transparent"
+    main={(
+      <View style={styles.root}>
+        <BackButton testID="back-button" style={styles.back} variant="text" label={<Icon style={styles.backIcon} name="arrow-back" />} />
+
+        <SearchBar style={styles.searchBar} onChangeText={onSearchChange} />
+      </View>
+    )}
+  />
+)
+
+export default SearchNavbar
