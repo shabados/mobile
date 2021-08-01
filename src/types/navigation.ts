857141fb12c56c013dffb233ef2e54531d0aab10
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-07-31 23:03:03 -0400

    feat: navigate from bookmark to gurbani screen

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,9 +1,11 @@
 import Screens from '../lib/screens'
 import { Folder } from '../components/Bookmarks/types'
 
+import { ContentTypes } from './data'
+
 export type NavigationParams = {
   [Screens.Bookmarks]: {currentFolder?: string, folderData: Folder[]},
   [Screens.Home]: undefined,
   [Screens.Search]: undefined,
-  [Screens.Gurbani]: undefined,
+  [Screens.Gurbani]: { id: string, type: ContentTypes },
 }
