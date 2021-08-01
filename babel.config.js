44b163aca708ae8b5d3d1b5ba32c7fe9732f523b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-07-31 23:08:40 -0400

    build: add remove console transform
    we only keep in dev
    https://reactnative.dev/docs/performance#using-consolelog-statements

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   presets: [ 'module:metro-react-native-babel-preset' ],
   plugins: [ [ '@babel/plugin-proposal-decorators', { legacy: true } ] ],
+  env: {
+    production: {
+      plugins: [ 'transform-remove-console' ],
+    },
+    test: {
+      plugins: [ 'transform-remove-console' ],
+    },
+  },
 }
