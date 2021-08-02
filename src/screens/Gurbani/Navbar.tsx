d149c803e698e18f407516dd67bc8047a8297fd8
Bhajneet S.K <bhajneet@gmail.com>
2021-08-02 12:25:28 -0400

    feat: replace top right main view icon with reader toggle

--- src/screens/Gurbani/Navbar.tsx
+++ src/screens/Gurbani/Navbar.tsx
@@ -1,43 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 
 import logo from '../../../assets/images/logo.png'
 import Colors from '../../themes/colors'
-import { px } from '../../themes/utils'
+import { px, py, my } from '../../themes/utils'
 import Typography from '../../components/Typography'
 import Navbar from '../../components/Navbar'
+import ReaderToggle from '../../components/ReaderToggle'
 
 const styles = StyleSheet.create( {
+  disabled: {
+    color: Colors.Disabled,
+  },
   headerIcon: {
     fontSize: 22,
-    color: Colors.Disabled,
-    paddingLeft: 20,
-    paddingRight: 20,
+    color: Colors.PrimaryText,
+    ...py( 20 ),
   },
   heading: {
     ...px( 6 ),
   },
   logo: {
-    width: 25,
-    height: 25,
+    width: 24,
+    height: 24,
+    ...my( 6 ),
   },
 } )
 
 /**
  * Navbar component for main header.
  */
-const GurbaniNavbar = () => (
-  <Navbar
-    left={<Icon style={styles.headerIcon} name="menu" />}
-    right={<Icon style={styles.headerIcon} name="ios-options-outline" />}
-    main={(
-      <>
-        <Image style={styles.logo} source={logo} />
-        <Typography variant="header" style={styles.heading}>Shabad OS</Typography>
-      </>
+const GurbaniNavbar = () => {
+  const [ isEnabled, setIsEnabled ] = useState( false )
+  const setToggle = () => setIsEnabled( !isEnabled )
+
+  return (
+    <Navbar
+      left={<Icon style={[ styles.headerIcon, styles.disabled ]} name="menu" />}
+      right={<ReaderToggle isEnabled={isEnabled} onChange={setToggle} />}
+      main={(
+        <>
+          <Image style={styles.logo} source={logo} />
+          <Typography variant="subtitle" style={styles.heading}>{isEnabled ? 'True' : 'False'}</Typography>
+        </>
     )}
-  />
-)
+    />
+  )
+}
 
 export default GurbaniNavbar
