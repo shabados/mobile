f9f9b95b2963ff8ffb39bed27c25a5fc88ec6341
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-08-06 00:13:44 -0400

    refactor: use convert collections to TS from json

---
+++ src/defaults/collections.ts
@@ -0,0 +1,51 @@
+import { ContentTypes } from '../types/data'
+
+export default [
+  {
+    name: 'Nitnem',
+    id: '0',
+    type: ContentTypes.Bani,
+    bookmarks: [
+      { id: '1', type: ContentTypes.Bani, name: 'Jap Ji Sahib' },
+      { id: '2', type: ContentTypes.Bani, name: 'Jaap Sahib' },
+      { id: '3', type: ContentTypes.Bani, name: 'Tav Prasad Savaiye (Sravag Sudh)' },
+      { id: '4', type: ContentTypes.Bani, name: 'Benti Chaupai Sahib' },
+      { id: '5', type: ContentTypes.Bani, name: 'Anand Sahib' },
+      { id: '6', type: ContentTypes.Bani, name: 'Rehras Sahib' },
+      { id: '7', type: ContentTypes.Bani, name: 'Ardaas' },
+      { id: '8', type: ContentTypes.Bani, name: 'Sohila Sahib' },
+    ],
+  },
+  {
+    name: 'Sundar Gutka',
+    id: '0',
+    type: ContentTypes.Bani,
+    bookmarks: [
+      { id: '1', type: ContentTypes.Bani, name: 'Jap Ji Sahib' },
+      { id: '2', type: ContentTypes.Bani, name: 'Jaap Sahib' },
+      { id: '3', type: ContentTypes.Bani, name: 'Tav Prasad Savaiye (Sravag Sudh)' },
+      { id: '4', type: ContentTypes.Bani, name: 'Benti Chaupai Sahib' },
+      { id: '5', type: ContentTypes.Bani, name: 'Anand Sahib' },
+      { id: '6', type: ContentTypes.Bani, name: 'Shabad Hazare' },
+      { id: '7', type: ContentTypes.Bani, name: 'Barah Maha' },
+      { id: '8', type: ContentTypes.Bani, name: 'Shabad Hazare Patshahi 10' },
+      { id: '9', type: ContentTypes.Bani, name: 'Tav Prasad Savaiye (Deenan Ki)' },
+      { id: '10', type: ContentTypes.Bani, name: 'Rehras Sahib' },
+      { id: '11', type: ContentTypes.Bani, name: 'Ardaas' },
+      { id: '12', type: ContentTypes.Bani, name: 'Aarti' },
+      { id: '13', type: ContentTypes.Bani, name: 'Sohila Sahib' },
+      { id: '14', type: ContentTypes.Bani, name: 'Bavan Akhri' },
+      { id: '15', type: ContentTypes.Bani, name: 'Sukhmani Sahib' },
+      { id: '16', type: ContentTypes.Bani, name: 'Asa Ki Var' },
+      { id: '17', type: ContentTypes.Bani, name: 'Oankaar' },
+      { id: '18', type: ContentTypes.Bani, name: 'Sidh Gosht' },
+      { id: '19', type: ContentTypes.Bani, name: 'Lavan (Anand Karaj)' },
+      { id: '20', type: ContentTypes.Bani, name: 'Chandi Di Var' },
+      {
+        id: '21',
+        type: ContentTypes.Bani,
+        name: 'Sri Guru Granth Sahib Paath Bhog (Ragmala)',
+      },
+    ],
+  },
+]
