c1d4c5e27d6f2c7df75128ae9397860a132bbd55
Harjot Singh <harjot@harkul.com>
2021-11-16 23:22:22 +0000

    style: add missing spaces to TypeScript definitions

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,7 +2,7 @@ import Screens from '../lib/screens'
 import { Folder } from '../components/Bookmarks/types'
 
 export type NavigationParams = {
-  [Screens.Bookmarks]: {currentFolder?: string, folderData: Folder[]},
+  [Screens.Bookmarks]: { currentFolder?: string, folderData: Folder[] },
   [Screens.Home]: undefined,
   [Screens.Search]: undefined,
   [Screens.Gurbani]: undefined,
