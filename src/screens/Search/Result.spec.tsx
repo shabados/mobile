ab77f86b90ae2049a899b1fdbe5aaa910b94e346
Harjot Singh <harjot@harkul.com>
2021-11-21 18:08:31 +0000

    test: refactor tests to use common setup function

--- src/screens/Search/Result.spec.tsx
+++ src/screens/Search/Result.spec.tsx
@@ -3,36 +3,32 @@ import { render, fireEvent } from '@testing-library/react-native'
 
 import wrapper from '../../../test/utils/NavigatorContext'
 
-import Result from './Result'
+import Result, { ResultProps } from './Result'
+
+const setup = ( props: Partial<ResultProps> ) => render(
+  <Result
+    gurmukhi="line"
+    source="source"
+    page="AMg 3"
+    translation="translation"
+    {...props}
+  />,
+  { wrapper },
+)
 
 describe( '<SearchResult />', () => {
   it( 'should render the full page number', () => {
-    const { getByText } = render(
-      <Result
-        gurmukhi="line"
-        source="source"
-        page="AMg 1"
-        translation="translation"
-      />, { wrapper },
-    )
+    const { getByText } = setup( { page: 'AMg 1' } )
 
     expect( getByText( 'ਅੰਗ ੧' ) ).toBeTruthy()
   } )
 
   it( 'given a press, should fire onPress', () => {
     const onPress = jest.fn()
-
-    const { getByText } = render(
-      <Result
-        gurmukhi="line"
-        source="source"
-        page="AMg 1"
-        translation="translation"
-        onPress={onPress}
-      />, { wrapper },
-    )
+    const { getByText } = setup( { onPress } )
 
     fireEvent.press( getByText( 'translation' ) )
+
     expect( onPress ).toHaveBeenCalled()
   } )
 } )
