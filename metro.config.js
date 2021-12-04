863f37daa6ba812b461aae31e755b5dacc54c625
Harjot Singh <harjot@harkul.com>
2021-12-04 20:24:33 +0000

    build: remove ts-jest and unused babel transforms

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   transformer: {
-    getTransformOptions: async () => ({
+    getTransformOptions: async () => ( {
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
-    }),
+    } ),
   },
-};
+}
