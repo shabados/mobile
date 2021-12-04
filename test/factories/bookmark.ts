69e378db85999b5fae6cba3a0bcb56ca43e706c5
Harjot Singh <harjot@harkul.com>
2021-12-04 20:22:36 +0000

    test: update factories to use datatype over deprecated random methods

--- test/factories/bookmark.ts
+++ test/factories/bookmark.ts
@@ -1,5 +1,5 @@
 import { Factory } from 'fishery'
-import { name, random } from 'faker'
+import { name, random, datatype } from 'faker'
 
 import { BookmarkData } from '../../src/types/data'
 
@@ -9,13 +9,13 @@ export const bookmark = Factory.define<BookmarkData>( ( { sequence } ) => ( {
   id: `bookmark-${sequence}`,
   nameGurmukhi: random.word(),
   source: {
-    id: random.number( 10 ),
-    length: random.number( 1500 ),
+    id: datatype.number( 10 ),
+    length: datatype.number( 1500 ),
     nameGurmukhi: random.words( 3 ),
     pageNameGurmukhi: random.word(),
   },
   writer: {
-    id: random.number( 10 ),
+    id: datatype.number( 10 ),
     nameGurmukhi: name.firstName(),
   },
   lines: line.buildList( 20 ),
