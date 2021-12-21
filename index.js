b2d5faa498488387714442ca45199c41e697420f
Harjot Singh <harjot@harkul.com>
2021-12-21 19:00:12 +0000

    refactor: remove app.json and use generic "App" component name

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import { enableScreens } from 'react-native-screens'
 
-import { name as appName } from './app.json'
 import App from './src/App'
 
 enableScreens()
 
-AppRegistry.registerComponent( appName, () => App )
+AppRegistry.registerComponent( 'App', () => App )
