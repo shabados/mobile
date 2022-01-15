d5dbe6352cfc18e2f3ee2af49786c5475adceae4
Harjot Singh <harjot@harkul.com>
2022-01-15 16:50:27 +0000

    build: add react-native-device-info and NativeEventEmitter mocks

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -1,6 +1,8 @@
 // Mock out native animations
 import 'react-native-gesture-handler/jestSetup'
 
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock'
+
 jest.mock( 'react-native-reanimated', () => {
   const Reanimated = require( 'react-native-reanimated/mock' )
 
@@ -13,3 +15,7 @@ jest.mock( 'react-native-reanimated', () => {
 // Silence the warning:
 // Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock( 'react-native/Libraries/Animated/NativeAnimatedHelper' )
+
+jest.mock( 'react-native/Libraries/EventEmitter/NativeEventEmitter' )
+
+jest.mock( 'react-native-device-info', () => mockRNDeviceInfo )
