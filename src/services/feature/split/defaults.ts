8c393bcb1d63651109f6288159fe2433d8dcc323
Harjot Singh <harjot@harkul.com>
2022-01-15 15:21:21 +0000

    feat: add platform default attribute to feature service

--- src/services/feature/split/defaults.ts
+++ src/services/feature/split/defaults.ts
@@ -1,7 +1,9 @@
+import { Platform } from 'react-native'
+
 import { SplitAttributes, SplitFeatures } from './types'
 
 export const getDefaultAttributes = (): SplitAttributes => ( {
-
+  platform: Platform.OS,
 } )
 
 const defaultStatuses: { [Key in keyof SplitFeatures]: SplitFeatures[Key] } = {
--- src/services/feature/split/types.ts
+++ src/services/feature/split/types.ts
@@ -1,8 +1,11 @@
+import { PlatformOSType } from 'react-native'
+
 export type OffTreatment = 'off'
 export type ControlTreatment = 'control'
 export type DefaultTreatment = 'on' | OffTreatment
 
 export type SplitAttributes = {
+  platform: PlatformOSType,
 }
 
 export type SplitFeatures = {
