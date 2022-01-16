800c13ef769825490e86767348409da8d5dd277b
Harjot Singh <harjot@harkul.com>
2022-01-16 15:42:24 +0000

    feat: export features and attributes types from feature service adapter

--- src/services/feature/index.ts
+++ src/services/feature/index.ts
@@ -1,7 +1,15 @@
 import UseFeatureFlagFactory from './hooks'
 import splitAdapter from './split'
+import { FeatureFlagClient } from './types'
 
 export const {
   useFeatureEnabled,
   useFeatureStatus,
 } = UseFeatureFlagFactory( splitAdapter )
+
+type Adapter = typeof splitAdapter
+
+export type Features = Adapter extends FeatureFlagClient<infer Features, never> ? Features : never
+export type Attributes = Adapter extends FeatureFlagClient<Features, infer Attributes>
+  ? Attributes
+  : never
