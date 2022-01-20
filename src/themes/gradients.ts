50c1b12a96e6fcfdeb6d695aa1fdecaef1d8eb03
Bhajneet S.K <bhajneet@gmail.com>
2022-01-20 18:35:09 -0500

    fix: try to set transparency of bottom bar based on theme

--- src/themes/gradients.ts
+++ src/themes/gradients.ts
@@ -1,9 +1,15 @@
+import { Appearance } from 'react-native'
+
+import Colors from './colors'
+
 /**
  * The gradients are very specific to the consumption of react-native-linear-gradient.
  */
 export const gradients = {
   TransparentToBlack: {
-    colors: [ 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 1)' ],
+    colors: Appearance.getColorScheme() === 'light'
+      ? [ 'rgba(255, 255, 255, 0)', Colors.MainView ]
+      : [ 'rgba(0, 0, 0, 0)', Colors.MainView ],
     locations: [ 0.0, 0.6 ],
   },
 }
