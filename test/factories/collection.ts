7905667103df5954d545d9c6253772b3a1b67036
Harjot Singh <harjot@harkul.com>
2022-02-01 23:31:56 +0000

    build: move from faker to @fakerjs/faker

--- test/factories/collection.ts
+++ test/factories/collection.ts
@@ -1,11 +1,11 @@
-import { random } from 'faker'
+import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 
 import { CollectionData } from '../../src/types/data'
 
 const collectionBase = Factory.define<CollectionData>( ( { sequence } ) => ( {
   id: `item-${sequence}`,
-  nameGurmukhi: random.word(),
+  nameGurmukhi: faker.random.word(),
 } ) )
 
 export const collection = Factory.define<CollectionData>( () => ( {
