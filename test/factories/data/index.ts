363337bcda70b4693934d242eac6536e4fe5ffbd
Harjot Singh <harjot@harkul.com>
2022-02-02 00:08:36 +0000

    test: use sequence to select gurmukhi line

--- test/factories/data/index.ts
+++ test/factories/data/index.ts
@@ -1,7 +1,3 @@
-import { faker } from '@faker-js/faker'
-
 import shabad from './sggs-first-shabad.json'
 
 export const gurmukhiLines = shabad.map( ( { gurmukhi } ) => gurmukhi )
-
-export const randomGurmukhi = () => faker.helpers.randomize( gurmukhiLines )
--- test/factories/line.ts
+++ test/factories/line.ts
@@ -2,11 +2,12 @@ import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 
 import { LineData } from '../../src/types/data'
+import getArrayElement from '../utils/get-array-element'
 import * as data from './data'
 
 export const line = Factory.define<LineData>( ( { sequence } ) => ( {
   id: faker.datatype.uuid(),
-  gurmukhi: data.randomGurmukhi(),
+  gurmukhi: getArrayElement( data.gurmukhiLines, sequence ),
   translations: [ {
     translation: faker.random.words( 10 ),
     translationSourceId: faker.datatype.number(),
