e431493e6de902ed514b4206cca6eb0d04a1432b
Harjot Singh <harjot@harkul.com>
2022-02-13 16:22:34 +0000

    refactor: remove unnecessary Group from HomeNavigator

--- src/screens/HomeNavigator.tsx
+++ src/screens/HomeNavigator.tsx
@@ -6,25 +6,23 @@ import { HomeTabParams } from '../types/navigation'
 import GurbaniScreen from './Gurbani'
 import SettingsScreen from './Settings'
 
-const { Group, Navigator, Screen } = createMaterialTopTabNavigator<HomeTabParams>()
+const { Navigator, Screen } = createMaterialTopTabNavigator<HomeTabParams>()
 
 const initialLayout = { width: Dimensions.get( 'window' ).width }
 const EmptyTabBar = () => null
 
 const HomeNavigator = () => (
   <Navigator tabBar={EmptyTabBar} initialLayout={initialLayout}>
-    <Group>
-      <Screen
-        name="Home.Gurbani"
-        component={GurbaniScreen}
-        initialParams={{
-          id: '1YU',
-          type: ContentType.Shabad,
-        }}
-      />
+    <Screen
+      name="Home.Gurbani"
+      component={GurbaniScreen}
+      initialParams={{
+        id: '1YU',
+        type: ContentType.Shabad,
+      }}
+    />
 
-      <Screen name="Home.Settings" component={SettingsScreen} />
-    </Group>
+    <Screen name="Home.Settings" component={SettingsScreen} />
   </Navigator>
 )
 
