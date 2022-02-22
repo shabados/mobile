223386fdb127ffd39c778e677ae9db889fe587f1
Harjot Singh <harjot@harkul.com>
2022-02-22 00:47:14 +0000

    refactor: create Empty noop component

--- src/screens/HomeNavigator.tsx
+++ src/screens/HomeNavigator.tsx
@@ -1,17 +1,20 @@
+import { Layout } from '@react-navigation/elements'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { Dimensions } from 'react-native'
 
+import Empty from '../components/Empty'
 import { HomeTabParams } from '../types/navigation'
 import GurbaniNavigator from './GurbaniNavigator'
 import SettingsNavigator from './SettingsNavigator'
 
 const { Navigator, Screen } = createMaterialTopTabNavigator<HomeTabParams>()
 
-const initialLayout = { width: Dimensions.get( 'window' ).width }
-const EmptyTabBar = () => null
+const initialLayout: Partial<Layout> = {
+  width: Dimensions.get( 'window' ).width,
+}
 
 const HomeNavigator = () => (
-  <Navigator tabBar={EmptyTabBar} initialLayout={initialLayout}>
+  <Navigator tabBar={Empty} initialLayout={initialLayout}>
     <Screen name="Home.Tab.Gurbani" component={GurbaniNavigator} />
     <Screen name="Home.Tab.Settings" component={SettingsNavigator} />
   </Navigator>
