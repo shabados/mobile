796a31dc5422a2be31716ad4616e5b4addebcdba
Harjot Singh <harjot@harkul.com>
2022-03-06 01:24:47 +0000

    feat: add Settings screen title translation

--- src/screens/SettingsNavigator.tsx
+++ src/screens/SettingsNavigator.tsx
@@ -1,22 +1,33 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import BackButton from '../components/BackButton'
+import { registerTranslations, useTranslation } from '../services/i18n'
 import { SettingsStackParams } from '../types/navigation'
 import SettingsScreen from './Settings'
 
+const strings = registerTranslations( {
+  settingsTitle: {
+    'en-US': 'Settings',
+  },
+} )
+
 const { Navigator, Screen } = createNativeStackNavigator<SettingsStackParams>()
 
-const SettingsNavigator = () => (
-  <Navigator>
-    <Screen
-      name="Settings.View"
-      component={SettingsScreen}
-      options={{
-        title: 'Settings',
-        headerLeft: BackButton,
-      }}
-    />
-  </Navigator>
-)
+const SettingsNavigator = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Navigator>
+      <Screen
+        name="Settings.View"
+        component={SettingsScreen}
+        options={{
+          title: t( strings.settingsTitle ),
+          headerLeft: BackButton,
+        }}
+      />
+    </Navigator>
+  )
+}
 
 export default SettingsNavigator
