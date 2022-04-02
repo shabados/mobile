81660afe91a34d713990ad94339257c812334549
Harjot Singh <harjot@harkul.com>
2022-04-03 00:24:39 +0100

    fix: include test as a local environment

--- src/services/configuration/environment.ts
+++ src/services/configuration/environment.ts
@@ -1,11 +1,13 @@
 import get from './get'
 
 const LOCAL_ENV = 'local'
+const LOCAL_ENVIRONMENTS = [ LOCAL_ENV, 'test' ]
+
 const name = get( 'ENV_NAME' ).default( LOCAL_ENV ).asString()
 
 const environment = {
   name,
-  isLocal: name === LOCAL_ENV,
+  isLocal: LOCAL_ENVIRONMENTS.includes( name ),
 }
 
 export default environment
