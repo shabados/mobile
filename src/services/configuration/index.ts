be1afd1600b90a34e6edec4912c87bf6a3db64a5
Harjot Singh <harjot@harkul.com>
2022-04-03 03:29:00 +0100

    fix: revert back to requiring configuration variables
    Causing crash and makes more sense to be consistent

--- src/services/configuration/index.ts
+++ src/services/configuration/index.ts
@@ -1,10 +1,12 @@
 import environment from './environment'
 import get from './get'
-import sentry from './sentry'
 
 const configuration = {
   environment,
-  sentry,
+  sentry: {
+    dsn: get( 'SENTRY_DSN' ).required().asString(),
+    releaseName: get( 'SENTRY_RELEASE' ).required().asString(),
+  },
   split: {
     apiKey: get( 'SPLIT_API_KEY' ).required().asString(),
   },
--- src/services/configuration/sentry.ts
+++
@@ -1,9 +0,0 @@
-import environment from './environment'
-import get from './get'
-
-const sentry = {
-  dsn: get( 'SENTRY_DSN' ).required( !environment.isLocal ).asString(),
-  releaseName: get( 'SENTRY_RELEASE' ).required( !environment.isLocal ).asString(),
-}
-
-export default sentry
