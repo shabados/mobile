0b8bc464928160d06a539aa9daf6e5f0cc93fc45
Bhajneet S.K <bhajneet@gmail.com>
2022-04-03 18:44:40 -0400

    only show languages we are currently supporting

--- src/services/i18n/languages.ts
+++ src/services/i18n/languages.ts
@@ -1,11 +1,23 @@
+// language codes according to ISO 639‑1
+// country/locale codes according to 3166-2
 export const languages = {
   en: 'English (US)',
-  'en-GB': 'English (UK)',
-  pa: 'ਪੰਜਾਬੀ',
-  fr: 'Français',
-  de: 'Deutsch',
-  es: 'Español',
-  hi: 'हिन्दी',
+  // 'en-GB': 'English (UK)',
+  // pa: 'ਪੰਜਾਬੀ',
+  // 'pa-PK': 'پن٘جابی',
+  // ar: 'العربية', // Arabic
+  // fa: 'فارسی', // Persian / Dari
+  // hi: 'हिन्दी',
+  // ms: 'Bahasa Melayu', // Malaysian
+  // es: 'Español', // Spanish
+  // it: 'Italiano', // Italian
+  // de: 'Deutsch', // German
+  // fr: 'Français', // French
+  // el: 'Ελληνικά', // Greek
+  // jp: '日本語', // Japanese
+  // th: 'ภาษาไทย', // Thai
+  // sw: 'Kiswahili', // Swahili
+  // bn: 'বাংলা', // Bengali
 }
 
 export type Languages = keyof typeof languages
