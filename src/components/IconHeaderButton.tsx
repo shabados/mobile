63ea093bcd24b8eaad017d2557f1815b629bf128
Bhajneet S.K <bhajneet@gmail.com>
2022-04-29 10:25:26 -0400

    refactor: remove unused style

--- src/components/IconHeaderButton.tsx
+++ src/components/IconHeaderButton.tsx
@@ -1,26 +1,15 @@
-import { StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { HeaderButton } from 'react-navigation-header-buttons'
 
 import Colors from '../themes/colors'
 
-const styles = StyleSheet.create( {
-  native: {
-    margin: 0,
-  },
-} )
-
 export type IconHeaderButtonProps = {
   title: string,
   disabled?: boolean,
-  // To fix https://github.com/react-navigation/react-navigation/issues/10058
-  // Native Stack Navigators + HeaderBackButton has funky margin otherwise
-  native?: boolean,
 }
 
-const IconHeaderButton = ( { disabled, native = true, ...props }: IconHeaderButtonProps ) => (
+const IconHeaderButton = ( { disabled, ...props }: IconHeaderButtonProps ) => (
   <HeaderButton
-    style={native && styles.native}
     IconComponent={Icon}
     iconSize={28}
     color={( disabled ? Colors.Disabled : Colors.PrimaryText ) as string}
