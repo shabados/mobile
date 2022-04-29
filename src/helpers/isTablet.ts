38353baf3a11441605038e291f840dbf22f975b8
Bhajneet S.K <bhajneet@gmail.com>
2022-04-29 10:36:20 -0400

    fix: logic in isTablet

--- src/helpers/isTablet.ts
+++ src/helpers/isTablet.ts
@@ -2,6 +2,6 @@ import { Dimensions, Platform } from 'react-native'
 
 const window = Dimensions.get( 'window' )
 const { width, height } = window
-const isTablet = ( Platform.OS === 'ios' && Platform.isPad ) || ( Platform.OS === 'android' && ( ( width >= 960 && height >= 720 ) || ( width >= 720 && width >= 960 ) ) )
+const isTablet = ( Platform.OS === 'ios' && Platform.isPad ) || ( Platform.OS === 'android' && ( ( width >= 960 && height >= 720 ) || ( width >= 720 && height >= 960 ) ) )
 
 export default isTablet
