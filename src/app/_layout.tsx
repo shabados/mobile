b5cedcace9259454df87ca23f11fe4007a7b6793
Harjot Singh <harjot@harkul.com>
2024-04-20 01:55:41 +0100

    fix: prevent header from popping into safe area space on app launch

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,22 @@
 import { Stack } from 'expo-router'
+import { ComponentProps } from 'react'
 
+import Container from '~/components/atoms/Container'
 import Empty from '~/components/atoms/Empty'
+import { Colors } from '~/themes'
 import withContexts from '~/with-contexts'
+
+const tabScreenOptions = {
+  headerShown: true,
+  // Required to stop the occasional glitch where the header seems to shoot above the safe area
+  // Not really sure why, but rendering a Container with a top safe area seems to fix it
+  // Note: this adds a negligible amount of extra space above the header
+  header: () => <Container safeArea top style={{ backgroundColor: Colors.MainView }} />,
+} satisfies ComponentProps<typeof Stack>['screenOptions']
 
 const RootLayout = () => (
   <Stack screenOptions={{ presentation: 'modal', headerShown: false }}>
-    <Stack.Screen name="(tabs)" />
+    <Stack.Screen name="(tabs)" options={tabScreenOptions} />
     <Stack.Screen name="collections" />
 
     <Stack.Screen name="search" options={{ headerShown: true, header: Empty }} />
