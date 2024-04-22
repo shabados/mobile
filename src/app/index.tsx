edf2a4ef7f2a5397a50ac120414bcf9d125175e1
Harjot Singh <harjot@harkul.com>
2024-04-23 00:05:40 +0100

    fix: add correct type to lastContentPath

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Redirect } from 'expo-router'
+import { Href, Redirect } from 'expo-router'
 
 import { getLastContentPath } from '~/services/history/last-content-path'
 
 const lastContentPath = getLastContentPath()
 
 const Index = () => ( lastContentPath
-  ? <Redirect href={lastContentPath} />
+  ? <Redirect href={lastContentPath as Href<'/(tabs)/content/(content)/[type]/[id]'>} />
   : <Redirect href="/content/shabad/DMP" /> )
 
 export default Index
